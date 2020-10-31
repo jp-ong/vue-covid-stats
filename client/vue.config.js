02711add24ae4fc86fbbd909c5d04dd4445ad65a
John Paul Ong <jpong5202@gmail.com>
2020-10-31 22:27:45 +0800

    Directory fix

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,10 @@
 const path = require("path");
 module.exports = {
-  outputDir: path.resolve(__dirname, "../server/public"),
+  outputDir: path.resolve(__dirname, "../public"),
   devServer: {
     proxy: {
       "/api": {
         target: "http://localhost:5000",
-        changeOrigin: true,
       },
     },
   },
